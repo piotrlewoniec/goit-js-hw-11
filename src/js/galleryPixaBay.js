a082e2db8f9eea0298c533bf92e7553beef60454
piotrlewoniec <117780551+piotrlewoniec@users.noreply.github.com>
2023-05-17 10:48:41 +0200

    changed after mentor's first freedback

--- src/js/galleryPixaBay.js
+++ src/js/galleryPixaBay.js
@@ -17,33 +17,9 @@ import axios, { isCancel, AxiosError } from 'axios';
       if (event.target.nodeName !== 'IMG') {
         return;
       }
-      //this.instanceSimpleLightbox.refresh();
-      // this.instanceSimpleLightbox.open();
-
-      this.instanceSimpleLightbox.on('show.simplelightbox', function (e) {
-        console.log(e); // some usefull information
-      });
-      this.instanceSimpleLightbox.on('shown.simplelightbox', function (e) {
-        console.log(e); // some usefull information
-      });
-      this.instanceSimpleLightbox.on('close.simplelightbox', function (e) {
-        console.log(e); // some usefull information
-      });
-      this.instanceSimpleLightbox.on('closed.simplelightbox', function (e) {
-        console.log(e); // some usefull information
-      });
-
       this.instanceSimpleLightbox.on('error.simplelightbox', function (e) {
-        console.log(e); // some usefull information
+        Notiflix.Notify.failure(`${e}`);
       });
-      // let instanceSimpleLightbox = new SimpleLightbox('.gallery a');
-      // let instanceSimpleLightbox = new SimpleLightbox('.gallery a', {
-      //   captionsData: 'alt',
-      //   captionDelay: '250',
-      // });
-      // instanceSimpleLightbox.on('error.simplelightbox', function (e) {
-      //   console.log(e); // some usefull information
-      // });
     },
     galleryDisplay: function (loadMore) {
       if (loadMore) {
@@ -51,7 +27,7 @@ import axios, { isCancel, AxiosError } from 'axios';
       } else {
         Notiflix.Notify.success('Creating gallery...');
       }
-      let galleryItem = []; //array of images with html code
+      let galleryItem = [];
       for (const image of this.searchedData.hits) {
         const {
           webformatURL,
@@ -88,7 +64,6 @@ import axios, { isCancel, AxiosError } from 'axios';
       </div>
         `);
       }
-      //<a class="gallery__item" href="${original}">
       let galleryToPublish = galleryItem.map(image => image).join(''); //building one string from array
       this.galleryArea.insertAdjacentHTML('beforeend', galleryToPublish); //adding elements to ul
       if (loadMore) {
@@ -105,7 +80,6 @@ import axios, { isCancel, AxiosError } from 'axios';
     apiRestAxios: function (searchString, pagination, loadMore = false) {
       let searchedDataLocal = {};
       let pointerObj = this;
-      console.log(pagination);
       getData();
       async function getData() {
         try {
@@ -113,7 +87,6 @@ import axios, { isCancel, AxiosError } from 'axios';
             {
               method: 'get',
               baseURL: 'https://pixabay.com/api',
-              // url: 'https://pixabay.com/api/?key=34529652-cc8793dd1496f54354573213f&q=yellow+flowers&image_type=photo',
               params: {
                 key: '34529652-cc8793dd1496f54354573213f',
                 image_type: 'photo',
@@ -126,21 +99,11 @@ import axios, { isCancel, AxiosError } from 'axios';
             },
             { signal: AbortSignal.timeout(5000) }
           );
-          // handle success
-          console.log(response);
-          console.log(response.data);
-          console.log(response.status);
-          console.log(response.statusText);
-          console.log(response.headers);
-          console.log(response.config);
-          //console.log('response.lengt', response.data.hits.length);
           if (response.data.hits.length !== 0) {
             searchedDataLocal = response.data;
-            //console.log('searchDataLocal', searchedDataLocal);
           } else {
             searchedDataLocal = -1;
           }
-          //console.log('searchedDataLocal', searchedDataLocal);
           if (searchedDataLocal === -1 && !loadMore) {
             Notiflix.Notify.info(
               'Sorry, there are no images matching your search query. Please try again.'
@@ -152,7 +115,6 @@ import axios, { isCancel, AxiosError } from 'axios';
             );
           } else {
             pointerObj.searchedData = searchedDataLocal;
-            console.log('searchedData', pointerObj.searchedData);
             if (pagination < 13) {
               pointerObj.loadMoreBtn.style.display = '';
             }
@@ -164,31 +126,30 @@ import axios, { isCancel, AxiosError } from 'axios';
             pointerObj.galleryDisplay(loadMore);
           }
         } catch (error) {
-          console.log(error.toJSON());
+          const errorMn = error.toJSON();
+          Notiflix.Notify.failure(`${errorMn}`);
           if (error.response) {
             // The request was made and the server responded with a status code
             // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
+            Notiflix.Notify.failure(`${error.response.data}`);
+            Notiflix.Notify.failure(`${error.response.status}`);
+            Notiflix.Notify.failure(`${error.response.headers}`);
           } else if (error.request) {
             // The request was made but no response was received
             // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
             // http.ClientRequest in node.js
-            console.log(error.request);
+            Notiflix.Notify.failure(`${error.request}`);
           } else {
             // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
+            Notiflix.Notify.failure(`Error ${error.message}`);
           }
-          console.log(error.config);
+          Notiflix.Notify.failure(`${error.config}`);
         }
       }
     },
     formSubmit: function (event) {
       event.preventDefault();
       this.loadMoreBtn.style.display = 'none';
-      //console.log(event.currentTarget.elements[0].value);
-      //console.log(event.currentTarget.searchQuery.value);
       if (event.currentTarget.searchQuery.value !== '') {
         Notiflix.Notify.info('Submitting...');
         if (this.galleryArea.hasChildNodes()) {
@@ -385,102 +346,3 @@ import axios, { isCancel, AxiosError } from 'axios';
 
 // Nieskończone przewijanie
 // Zamiast przycisku «Load more» można zrobić nieskończone przewijanie obrazów podczas przewijania strony. Zapewniamy Ci pełną swobodę w realizacji, możesz użyć dowolnej biblioteki.
-
-//Backup test
-
-//standard:
-// axios
-//   .get(
-//     'https://pixabay.com/api/?key=34529652-cc8793dd1496f54354573213f&q=yellow+flowers&image_type=photo'
-//   )
-//   .then(function (response) {
-//     // handle success
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-//   .finally(function () {
-//     // always executed
-//   });
-// promise:
-// axios(
-//   {
-//     method: 'get',
-//     baseURL: 'https://pixabay.com/api',
-//     // url: 'https://pixabay.com/api/?key=34529652-cc8793dd1496f54354573213f&q=yellow+flowers&image_type=photo',
-//     params: {
-//       key: '34529652-cc8793dd1496f54354573213f',
-//       image_type: 'photo',
-//       orientation: 'horizontal',
-//       safesearch: true,
-//       per_page: 40,
-//       page: pagination,
-//       q: searchString,
-//     },
-//   },
-//   { signal: AbortSignal.timeout(5000) }
-// )
-//   .then(function (response) {
-//     // handle success
-//     console.log(response);
-//     console.log(response.data);
-//     console.log(response.status);
-//     console.log(response.statusText);
-//     console.log(response.headers);
-//     console.log(response.config);
-//     //console.log('response.lengt', response.data.hits.length);
-//     if (response.data.hits.length !== 0) {
-//       searchedDataLocal = response.data;
-//       //console.log('searchDataLocal', searchedDataLocal);
-//     } else {
-//       searchedDataLocal = -1;
-//     }
-//   })
-//   .catch(function (error) {
-//     console.log(error.toJSON());
-//     if (error.response) {
-//       // The request was made and the server responded with a status code
-//       // that falls out of the range of 2xx
-//       console.log(error.response.data);
-//       console.log(error.response.status);
-//       console.log(error.response.headers);
-//     } else if (error.request) {
-//       // The request was made but no response was received
-//       // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-//       // http.ClientRequest in node.js
-//       console.log(error.request);
-//     } else {
-//       // Something happened in setting up the request that triggered an Error
-//       console.log('Error', error.message);
-//     }
-//     console.log(error.config);
-//   })
-//   .finally(function () {
-//     // always executed
-//     //console.log('searchedDataLocal', searchedDataLocal);
-//     if (searchedDataLocal === -1) {
-//       Notiflix.Notify.info(
-//         'Sorry, there are no images matching your search query. Please try again.'
-//       );
-//       if (loadMore) {
-//         pointerObj.loadMoreBtn.style.display = 'none';
-//         Notiflix.Notify.info(
-//           "We're sorry, but you've reached the end of search results."
-//         );
-//       }
-//     } else {
-//       pointerObj.searchedData = searchedDataLocal;
-//       console.log('searchedData', pointerObj.searchedData);
-//       if (pagination < 13) {
-//         pointerObj.loadMoreBtn.style.display = '';
-//       }
-//       if (pagination === 1) {
-//         Notiflix.Notify.info(
-//           `Hooray! We found ${pointerObj.searchedData.totalHits} images.`
-//         );
-//       }
-//       pointerObj.galleryDisplay(loadMore);
-//     }
-//   });
